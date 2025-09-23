90bd16f4069933eced1ecbccbf27ca0876e3ab2e
Malik M. Ahsan <malikmuhammadahsan53@gmail.com>
2025-09-23 14:51:21 +0500

    chore: delete fungi-finders project from repo (moved to standalone repo)

--- JavaScript/Types/objects.js
+++
@@ -1,8 +0,0 @@
-// Objects are collections of key-value pairs. Everything else falls under objects
-const person = { name: "Alice" }; // Object
-const colors = ["red", "green"];  // Array
-function add(a, b) { return a + b; } // Function (a callable object)
-
-console.log(typeof person);
-console.log(typeof colors);
-console.log(typeof add);
